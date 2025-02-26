43c85d29ce0b030b9a7d24516270e2b3fd2f027d
Mits <hoanglonglpgxxx@gmail.com>
2025-02-26 16:26:53 +0700

    init project 04 eat-n-split

---
+++ 04-eat-n-split/src/App.jsx
@@ -0,0 +1,129 @@
+import PropTypes from "prop-types";
+import "./App.css";
+import { useState } from "react";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [showSplitBill, setShowSplitBill] = useState(false);
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList></FriendsList>
+        {showAddFriend && <AddFriendForm />}
+        <Button showAddFriend={showAddFriend} handleAddFriend={setShowAddFriend}>
+          {!showAddFriend ? "Add friend" : "Hide"}
+        </Button>
+      </div>
+      {showSplitBill && <FormSplitBill />}
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id}></Friend>
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owe you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+Friend.propTypes = {
+  friend: PropTypes.object.isRequired,
+};
+
+function AddFriendForm() {
+  return (
+    <form className="form-add-friend">
+      <label>👩🏼‍🤝‍🧑🏾Friend name</label>
+      <input type="text" />
+
+      <label>🌴Image URL</label>
+      <input type="text" />
+    </form>
+  );
+}
+
+function Button({ showAddFriend, handleAddFriend, children }) {
+  return (
+    <button className="button" onClick={() => handleAddFriend(!showAddFriend)}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  showAddFriend: PropTypes.bool.isRequired,
+  handleAddFriend: PropTypes.func.isRequired,
+  children: PropTypes.object.isRequired,
+};
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>💰 Ur expense</label>
+      <input type="text" />
+
+      <label>💰 X's expense</label>
+      <input type="text" disabled />
+
+      <label>💰 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
