3e3562a788b375bf6d40cfa57a9be951c47cc1f7
Mits <hoanglonglpgxxx@gmail.com>
2025-05-06 18:03:00 +0700

    up

--- 10-atomic-blog/src/App.js
+++ 10-atomic-blog/src/App.js
@@ -124,14 +124,16 @@ function List() {
   const { posts } = usePosts();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
--- 10-atomic-blog/src/Test.js
+++ 10-atomic-blog/src/Test.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 function SlowComponent() {
-  // If this is too slow on your maching, reduce the `length`
   const words = Array.from({ length: 100_000 }, () => "WORD");
   return (
     <ul>
@@ -14,13 +13,29 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
     </div>
   );
+}
+
+export default function Test() {
+  /* return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+      <SlowComponent />
+    </div>
+  ); */
+  return <div>
+    <h1>Slow counter?!?</h1>
+    <Counter>
+      <SlowComponent></SlowComponent>
+    </Counter>
+  </div>;
 }
