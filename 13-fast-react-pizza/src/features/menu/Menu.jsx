4ca9adb508a49e237c8423c85b41fa6d0f30b913
Mits <hoanglonglpgxxx@gmail.com>
2025-05-29 16:46:49 +0700

    fetch API to get menu with React Router Loader and render menu item

--- 13-fast-react-pizza/src/features/menu/Menu.jsx
+++ 13-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((item) => (
+        <MenuItem pizza={item} key={item.id}></MenuItem>
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
