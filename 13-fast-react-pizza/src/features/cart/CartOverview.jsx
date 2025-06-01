4a5cc39d9b2c3472531c61146a5f8ba100d2c381
Mits <hoanglonglpgxxx@gmail.com>
2025-06-01 21:14:25 +0700

    upupup

--- 13-fast-react-pizza/src/features/cart/CartOverview.jsx
+++ 13-fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,7 +1,7 @@
 function CartOverview() {
   return (
-    <div className="bg-stone-700 p-4 text-stone-200 uppercase">
-      <p className="space-x-4 font-semibold text-stone-300">
+    <div className="bg-stone-700 px-4 py-4 text-stone-200 uppercase sm:px-6">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
