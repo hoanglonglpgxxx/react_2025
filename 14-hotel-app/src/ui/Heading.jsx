60eb1d1a0e49576656f6dbd27487a124a7f9192c
Mits <hoanglonglpgxxx@gmail.com>
2025-12-01 16:37:27 +0700

    up, start trying to use supabase

--- 14-hotel-app/src/ui/Heading.jsx
+++ 14-hotel-app/src/ui/Heading.jsx
@@ -10,21 +10,18 @@ const Heading = styled.h1`
     css`
       font-size: 3rem;
       font-weight: 600;
-      background-color: red;
     `}
   ${(props) =>
     props.as === "h2" &&
     css`
       font-size: 2rem;
       font-weight: 600;
-      background-color: red;
     `}
   ${(props) =>
     props.as === "h3" &&
     css`
       font-size: 2rem;
       font-weight: 500;
-      background-color: red;
     `}
 
   line-height: 1.4
