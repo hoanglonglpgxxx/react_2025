204be0bcd36cb03757abc4e7688fcec7f2d745eb
Mits <hoanglonglpgxxx@gmail.com>
2025-12-02 16:03:35 +0700

    update

--- 14-hotel-app/src/ui/AppLayout.jsx
+++ 14-hotel-app/src/ui/AppLayout.jsx
@@ -15,6 +15,14 @@ const Main = styled.main`
   padding: 4rem 4.8rem 5.4rem;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function AppLayout() {
   // layout wrapper chung của toàn bộ
   return (
@@ -23,7 +31,9 @@ function AppLayout() {
       <Sidebar />
 
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
