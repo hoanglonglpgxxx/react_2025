f03c9e15b722d902f1c4fb6d7c9ca6fe33bd5db6
Mits <hoanglonglpgxxx@gmail.com>
2025-12-02 14:19:34 +0700

    done folder 26

---
+++ 14-hotel-app/src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://ckcsvgntzdukvgxevvyh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrY3N2Z250emR1a3ZneGV2dnloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1NjIyOTMsImV4cCI6MjA4MDEzODI5M30.0W1-viyG-R5bWGyrhHGXmnRF_uON2380mTGQYEcCBUU';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
